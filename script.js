22680c83c2172842eddd8bed73f0addbae5aa8a0
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-01-26 18:28:34 +0100

    Defined buttons functions and categories

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
+//mettere i todo in prima pagina come liste
+//Possibilità di editare un todo
+//Cancellare un todo (posso riciclare quello che ho già fatto)
+//Segnare un todo come fatto/non fatto
+//Spostare todo fatto dietro a quelli da fare, se si toggla un todo già fatto in non fatto si sposta sotto quelli da fare (vedi app promemoria)
+//Aprire finestra con un todo-->secondo me il modal bisognerebbe farlo interamente in javascript! modal per vedere dettagli e modal inserisci todo
+
 //DOM Stuff
-let todos = [];
-const newTodobtn = document.querySelector(".new-note");
+const newTodobtn = document.querySelector(".new-todo");
 const todoContainer = document.querySelector(".todo-container");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close");
+const btnAddTodo = document.querySelector(".add-todo");
+const dueDate = document.querySelector("#date");
 
-//Evento al bottone pr scrivere una nuova nota
-//newTodobtn.addEventListener("click", openNewTodoDialog);
-
-//Struttura di un todo
-class Todo {
-    constructor(title, desc, dueDate, priority) {
-        this.title = title;
-        this.desc = desc;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        //this.list = list;
-    }
-}
-
-//Helper function creare elementi delle carte
 function createCardElement(el, content) {
     const element = document.createElement(el);
     element.textContent = content;
@@ -32,18 +28,76 @@ function createTodoCard(newTodo) {
     article.appendChild(createCardElement("p", `${newTodo.desc}`));
     article.appendChild(createCardElement("p", `${newTodo.dueDate}`));
     article.appendChild(createCardElement("p", `${newTodo.priority}`));
+    article.appendChild(createCardElement("p", `${newTodo.category}`));
 }
-
-function addTodotoArray(title, desc, dueDate, priority) {
-    const newTodo = new Todo(title, desc, dueDate, priority);
-    todos.push(newTodo);
-    return newTodo;
-}
-
-addTodotoArray("Prova", "descrizione", "domani", "alta");
 
 function showTodos() {
     todos.map(element => createTodoCard(element));
 }
 
+//Evento al bottone per scrivere una nuova nota
+newTodobtn.addEventListener("click", openNewTodoDialog);
+
+function openNewTodoDialog(event) {
+    event.preventDefault();
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+btnCloseModal.addEventListener("click", closeTodoDialog);
+
+function closeTodoDialog(event) {
+    event.preventDefault();
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+btnAddTodo.addEventListener("click", event => {
+    event.preventDefault();
+    const newTodo = addTodotoArray(title.value, desc.value, dueDate.value, priority.value, category.value);
+    createTodoCard(newTodo);
+});
+
+//Struttura di un todo
+class Todo {
+    constructor(title, desc, dueDate, priority, category, status) {
+        this.title = title;
+        this.desc = desc;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.category = category || "Personal";
+        this.status = status || false;
+    }
+
+    //funzione che tiene conto dello status fatto/non fatto
+}
+
+let todos = [];
+let categories = ["Personal", "Work"]
+
+function addTodotoArray(title, desc, dueDate, priority, category) {
+    const newTodo = new Todo(title, desc, dueDate, priority, category);
+    todos.push(newTodo);
+    return newTodo;
+}
+
+function showCategories() {
+    categories.map(element => {
+        const option = document.createElement("option");
+        const selectCategory = document.querySelector("#category");
+        option.textContent = `${element}`;
+        option.value = `${element}`;
+        selectCategory.appendChild(option);
+    })
+}
+
+function addNewCategory(cat) {
+    categories.push(cat);
+    return categories;
+}
+
+//Elemento di prova
+addTodotoArray("Prova", "descrizione", "domani", "alta");
+
 showTodos();
+showCategories();
